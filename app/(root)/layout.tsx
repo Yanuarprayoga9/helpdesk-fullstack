e190391083a9d5d948228a7de4bae9a21fb678a9
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-13 19:22:58 +0700

    feat: instal sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,28 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import { MainNav } from "@/components/main-nav"
+import { Search } from "@/components/search"
+import TeamSwitcher from "@/components/team-switcher"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { UserNav } from "@/components/user-nav"
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main>
-                <SidebarTrigger />
+            <main className="w-full">
+                <div className=" border-b">
+                    <div className=" flex h-16 items-center px-4">
+                        <SidebarTrigger />
+                        <TeamSwitcher />
+
+                        <MainNav className="mx-6" />
+                        <div className="ml-auto flex items-center space-x-4">
+                            <Search />
+                            <UserNav />
+                        </div>
+                    </div>
+                </div>
                 {children}
             </main>
         </SidebarProvider>
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -45,7 +45,7 @@ export function AppSidebar() {
         <Sidebar>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupLabel className="p-6 text-sm font-bold">Application</SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
                             {items.map((item) => (
