36182b31e06351bf1a37140994ad6da5c27b4d1d
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-15 14:21:46 +0700

    feat: role formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatStringRoles = (roles: string[] | undefined) => {
+  if (roles == undefined)
+    return null
+  if (roles.length === 0) return "";
+  if (roles.length === 1) return roles[0];
+
+  const lastRole = roles.pop();
+  return `${roles.join(", ")} and ${lastRole}`;
+};
+
