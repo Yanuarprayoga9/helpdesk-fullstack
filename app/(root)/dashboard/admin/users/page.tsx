92c3a006f9701ef6cca88fec801fa84a2a339785
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-25 18:39:14 +0700

    feat: user table

--- app/(root)/dashboard/admin/users/page.tsx
+++ app/(root)/dashboard/admin/users/page.tsx
@@ -1,9 +1,14 @@
+import { DataTable } from '@/components/data-table'
 import React from 'react'
+import { columns } from './components/columns'
+import { getUsers } from '@/actions/users'
 
-const page = () => {
+const page = async () => {
+  const { users } = await getUsers()
+  console.log(users)
   return (
     <div>
-      
+      <DataTable searchKey='id' columns={columns} data={users || []} />
     </div>
   )
 }
