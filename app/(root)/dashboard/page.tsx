68eaecc1661abb47d9e55d635f3c24c3aeeff7a6
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-25 19:14:43 +0700

    feat: flex userpage

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 // import { getUsers } from "@/actions/users";
 import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 import { signOut } from "@/lib/auth";
 import Link from "next/link";
 import React, { Suspense } from "react";
@@ -9,16 +10,21 @@ const page = async () => {
 
   return (
     <Suspense fallback="Loading...">
-      <form
-        action={async () => {
-          "use server"
-          await signOut()
-        }}
-      >
-        <button type="submit">logout</button>
-      </form>
-      <Link href={"/dashboard/admin"}>tes</Link>
-      <ModeToggle />
+      <div className="flex justify-between space-x-4">
+        <form
+          action={async () => {
+            "use server"
+            await signOut()
+          }}
+        >
+          <Button type="submit" className="bg-red-500">logout</Button>
+        </form>
+        <Link href={"/dashboard/admin/users"}>redirect to users</Link>
+        <div>
+          <h1>toggle</h1>
+          <ModeToggle />
+        </div>
+      </div>
     </Suspense>
   )
 };
