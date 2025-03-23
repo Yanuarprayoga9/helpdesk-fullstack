d153d7d87465390770eb5ed07e35b7534ff0e6dc
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-03-23 12:06:17 +0700

    fix: params mpromise

--- app/(root)/dashboard/[loggedUserId]/profile/edit/page.tsx
+++ app/(root)/dashboard/[loggedUserId]/profile/edit/page.tsx
@@ -3,27 +3,23 @@ import { PasswordUpdateForm } from "../components/password-profile-form";
 import { getUserById } from "@/actions/user";
 
 type PageParams = {
-    params: {
-        loggedUserId: string;
-    };
+  params: Promise<{ loggedUserId: string }>;
 };
 
-const ProfilePage = async ({ params }: PageParams) => {
-    const { loggedUserId } = params;
-    const response = await getUserById(loggedUserId);
-    const me = response.success ? response.user : null;
+export default async function ProfilePage({ params }: PageParams) {
+  const { loggedUserId } = await params; // Awaiting the params Promise
+  const response = await getUserById(loggedUserId);
+  const me = response.success ? response.user : null;
 
-    return (
-        <div className="container min-w-96 mx-auto p-4 space-y-8">
-            <h1 className="text-2xl font-bold">Profile Settings</h1>
+  return (
+    <div className="container min-w-96 mx-auto p-4 space-y-8">
+      <h1 className="text-2xl font-bold">Profile Settings</h1>
 
-            {/* Form untuk update data umum */}
-            <GeneralProfileForm user={me} />
+      {/* Form to update general data */}
+      <GeneralProfileForm user={me} />
 
-            {/* Form untuk update password */}
-            <PasswordUpdateForm userId={me?.id || ""} />
-        </div>
-    );
-};
-
-export default ProfilePage;
+      {/* Form to update password */}
+      <PasswordUpdateForm userId={me?.id || ""} />
+    </div>
+  );
+}
