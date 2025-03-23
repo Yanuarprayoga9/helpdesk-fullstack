81bb3d505c5542b3135b5e9ea294ae01058c71f3
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-03-23 11:58:56 +0700

    fix: style of imports

--- app/(root)/dashboard/[loggedUserId]/profile/edit/page.tsx
+++ app/(root)/dashboard/[loggedUserId]/profile/edit/page.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { GeneralProfileForm } from '../components/general-profile-form'
-import { PasswordUpdateForm } from '../components/password-profile-form'
-import { getUserById } from '@/actions/user'
+import { GeneralProfileForm } from "../components/general-profile-form";
+import { PasswordUpdateForm } from "../components/password-profile-form";
+import { getUserById } from "@/actions/user";
+
 type PageParams = {
     params: {
-        loggedUserId: string
-    },
-}
+        loggedUserId: string;
+    };
+};
 
-const ProfilePage: React.FC<PageParams> = async ({ params }) => {
-        const { loggedUserId } =  params; // Tambahkan await
-        const response = await getUserById(loggedUserId);
-        const me = response.success ? response.user : null;
+const ProfilePage = async ({ params }: PageParams) => {
+    const { loggedUserId } = params;
+    const response = await getUserById(loggedUserId);
+    const me = response.success ? response.user : null;
+
     return (
-    <div className="container min-w-96  mx-auto p-4 space-y-8">
+        <div className="container min-w-96 mx-auto p-4 space-y-8">
             <h1 className="text-2xl font-bold">Profile Settings</h1>
 
             {/* Form untuk update data umum */}
-            <GeneralProfileForm user={me } />
+            <GeneralProfileForm user={me} />
 
             {/* Form untuk update password */}
-            <PasswordUpdateForm  userId={me?.id || ""}/>
+            <PasswordUpdateForm userId={me?.id || ""} />
         </div>
-  )
-}
+    );
+};
 
-export default ProfilePage
-
+export default ProfilePage;
